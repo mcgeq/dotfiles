ce12839aa69adf6a3315e04d89622dc628868bb2
mcge <mcgeq@outlook.com>
2024-06-15 16:05:35 +0800

    :wrench: config(config): Add eslint vitest

--- mcge-commit/eslint.config.js
+++ mcge-commit/eslint.config.js
@@ -4,6 +4,7 @@ import tseslint from 'typescript-eslint';
 import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js';
 import { fixupConfigRules } from '@eslint/compat';
 import pluginPrettierRecommendedConfigs from 'eslint-plugin-prettier/recommended';
+import vitest from 'eslint-plugin-vitest';
 
 export default [
 	{ languageOptions: { globals: { ...globals.browser, ...globals.es2020, ...globals.node } } },
@@ -11,9 +12,28 @@ export default [
 	...tseslint.configs.recommended,
 	pluginPrettierRecommendedConfigs,
 	{
+		files: ['test/**'],
+		plugins: {
+			vitest,
+		},
+		rules: {
+			...vitest.configs.recommended.rules,
+			'vitest/max-nested-describe': ['error', { max: 3 }],
+		},
+		settings: {
+			vitest: {
+				typecheck: true,
+			},
+		},
+		languageOptions: {
+			globals: {
+				...vitest.environments.env.globals,
+			},
+		},
+	},
+	{
 		files: ['**/*.jsx', '**/*.tsx'],
 		languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } },
 	},
 	...fixupConfigRules(pluginReactConfig),
 ];
-
