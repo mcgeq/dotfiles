ebda60a49b8b820dff3dabe072a07c6fdd14d81b
mcge <mcgeq@outlook.com>
2025-01-04 11:43:33 +0800

    feat(nvim): :art: Add rustaceanvim

--- mcge-nvim/lua/mcge/dap/nvim-dap/config/rust.lua
+++ mcge-nvim/lua/mcge/dap/nvim-dap/config/rust.lua
@@ -1,4 +1,4 @@
--- lua/insis/lsp/config/rust.lua
+-- lua/mcge/lsp/config/rust.lua
 -- TODO: change path
 local extension_path =
   "/Users/nn/.local/share/nvim/site/pack/packer/start/vimspector/gadgets/macos/download/CodeLLDB/v1.6.10/root/extension/"
@@ -6,5 +6,5 @@ local codelldb_path = extension_path .. "adapter/codelldb"
 local liblldb_path = extension_path .. "lldb/lib/liblldb.dylib"
 
 return {
-  adapter = require("rust-tools.dap").get_codelldb_adapter(codelldb_path, liblldb_path),
+   adapter = require("rust-tools.dap").get_codelldb_adapter(codelldb_path, liblldb_path),
 }
--- mcge-nvim/lua/mcge/lsp/config/rust.lua
+++ mcge-nvim/lua/mcge/lsp/config/rust.lua
@@ -1,37 +1,36 @@
 local common = require("mcge.lsp.common-config")
 local opts = {
-  capabilities = common.capabilities,
-  flags = common.flags,
-  on_attach = function(client, bufnr)
-    common.disableFormat(client)
-    common.keyAttach(bufnr)
-  end,
-  settings = {
-    -- to enable rust-analyzer settings visit:
-    -- https://github.com/rust-analyzer/rust-analyzer/blob/master/docs/user/generated_config.adoc
-    ["rust-analyzer"] = {
-      -- enable clippy on save
-      checkOnSave = {
-        command = "clippy",
-      },
-    },
-  },
+	capabilities = common.capabilities,
+	flags = common.flags,
+	on_attach = function(client, bufnr)
+		common.disableFormat(client)
+		common.keyAttach(bufnr)
+	end,
+	settings = {
+		-- to enable rust-analyzer settings visit:
+		-- https://github.com/rust-analyzer/rust-analyzer/blob/master/docs/user/generated_config.adoc
+		["rust-analyzer"] = {
+			-- enable clippy on save
+			checkOnSave = {
+				command = "clippy",
+			},
+		},
+	},
 }
 
 return {
-  on_setup = function(server)
-    local ok_rt, rust_tools = pcall(require, "rust-tools")
-    if not ok_rt then
-      print("Failed to load rust tools, will set up `rust_analyzer` without `rust-tools`.")
-      server.setup(opts)
-    else
-      -- We don't want to call lspconfig.rust_analyzer.setup() when using rust-tools
-      rust_tools.setup({
-        server = opts,
-        -- NOTICE: require dap 
-        dap = require("mcge.dap.nvim-dap.config.rust"),
-      })
-    end
-  end,
+	on_setup = function(server)
+		local ok_rt, rustaceanvim = pcall(require, "rustaceanvim")
+		if not ok_rt then
+			print("Failed to load rust tools, will set up `rust_analyzer` without `rustaceanvim`.")
+			server.setup(opts)
+		--else
+			-- We don't want to call lspconfig.rust_analyzer.setup() when using rust-tools
+			-- rustaceanvim.setup({
+			--	server = opts,
+				-- NOTICE: require dap
+				-- dap = require("mcge.dap.nvim-dap.config.rust"),
+			--})
+		end
+	end,
 }
-
