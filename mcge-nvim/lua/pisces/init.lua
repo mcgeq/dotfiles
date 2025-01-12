ab4676c37cd211a84e2bd121913063eb90a73a71
mcge <mcgeq@outlook.com>
2025-01-12 14:52:05 +0800

    feat(nvim): :art: Modify lazygit keymap.

--- mcge-nvim/lua/pisces/init.lua
+++ mcge-nvim/lua/pisces/init.lua
@@ -5,27 +5,34 @@ M.version = "0.1.0"
 M.config = require("pisces.config")
 --- @param user_config UserConfig
 function M.setup(user_config)
-  require("pisces.utils.global")
-  require("pisces.options")
-  -- user config override
-  M.config = vim.tbl_deep_extend("force", M.config, user_config)
-  require("pisces.env").init(M.config)
-  require("pisces.keymaps")
-  local pluginManager = require("pisces.lazy")
-  if not pluginManager.avaliable() then
-    pluginManager.install()
-  end
-  pluginManager.setup()
-  require("pisces.colorscheme").reset()
-  require("pisces.autocmds")
-  require("pisces.lsp")
-  require("pisces.cmp")
-  require("pisces.format")
-  require("pisces.dap")
-  require("pisces.utils.color-preview")
-  if M.config.fix_windows_clipboard then
-    require("utils.fix-yank")
-  end
+	require("pisces.utils.global")
+	require("pisces.options")
+	-- user config override
+	M.config = vim.tbl_deep_extend("force", M.config, user_config)
+	require("pisces.env").init(M.config)
+	require("pisces.keymaps")
+	local pluginManager = require("pisces.lazy")
+	if not pluginManager.avaliable() then
+		pluginManager.install()
+	end
+	pluginManager.setup()
+	require("pisces.colorscheme").reset()
+	require("pisces.autocmds")
+	require("pisces.lsp")
+	require("pisces.cmp")
+	require("pisces.format")
+	require("pisces.dap")
+	require("pisces.utils.color-preview")
+	if M.config.fix_windows_clipboard then
+		require("utils.fix-yank")
+	end
 end
+
+-- vim.api.nvim_create_autocmd("BufEnter", {
+-- 	pattern = "*",
+-- 	callback = function()
+-- 		require("lazygit.utils").project_root_dir()
+-- 	end,
+-- })
 
 return M
--- mcge-nvim/lua/pisces/plugins/init.lua
+++ mcge-nvim/lua/pisces/plugins/init.lua
@@ -350,7 +350,7 @@ return {
 		-- setting the keybinding for LazyGit with 'keys' is recommended in
 		-- order to load the plugin when the command is run for the first time
 		keys = {
-			{ "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+			{ "<leader>lg", "<cmd>LazyGitCurrentFile<cr>", desc = "LazyGit" },
 		},
 		config = function()
 			require("telescope").load_extension("lazygit")
