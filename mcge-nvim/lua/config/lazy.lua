2196caeab2581d7bd919bb1ca53d362286ac3d89
mcgeq <mcgeq@outlook.com>
2025-03-01 19:21:10 +0800

    feat(nvim): :art: Add extras

--- mcge-nvim/lua/config/lazy.lua
+++ mcge-nvim/lua/config/lazy.lua
@@ -19,6 +19,9 @@ require("lazy").setup({
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import/override with your plugins
+    { import = "lazyvim.plugins.extras.coding.blink" },
+    { import = "lazyvim.plugins.extras.coding.mini-comment" },
+    { import = "lazyvim.plugins.extras.coding.mini-surround" },
     { import = "lazyvim.plugins.extras.lang.rust" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.clangd" },
