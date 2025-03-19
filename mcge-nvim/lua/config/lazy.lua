65ec35a516f8e2293dcea0e51bdc757841f4fbd4
mcgeq <mcgeq@outlook.com>
2025-03-19 20:11:33 +0800

    feat(nvim): :art: Add snippets

--- mcge-nvim/lua/config/lazy.lua
+++ mcge-nvim/lua/config/lazy.lua
@@ -22,7 +22,6 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.coding.blink" },
     { import = "lazyvim.plugins.extras.coding.mini-comment" },
     { import = "lazyvim.plugins.extras.coding.mini-surround" },
-    { import = "lazyvim.plugins.extras.coding.mini-snippets" },
     { import = "lazyvim.plugins.extras.lang.rust" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.clangd" },
