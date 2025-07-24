e7878dcbad75c9a7e1a8d314fb3283248049855b
mcgeq <mcgeq@outlook.com>
2025-07-24 22:20:40 +0800

    feat(nvim): :fire: Remove lazynim config

--- mcge-nvim/lua/config/options.lua
+++
@@ -1,6 +0,0 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
-vim.g.lazyvim_rust_diagnostics = "rust-analyzer"
-vim.g.lazyvim_prettier_needs_config = true
-vim.g.lazyvim_mini_snippets_in_completion = true
